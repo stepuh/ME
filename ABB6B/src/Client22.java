1964b216d59c40b5b70123619b760cff499fae88
Sigi <s.gessulat@gmail.com>
2011-12-09 16:49:17 +0100

    Aufgabe1 done.

---
+++ ABB6B/src/Client22.java
@@ -0,0 +1,47 @@
+
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+
+
+public class Client22 {
+
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+		ArrayList<Dataset> training = new Reader("digits-testing-neu.txt").getDatasets();
+		int threadsN = 1;
+		
+		// do preprocessing: remove Datasets with class -1
+		ArrayList<Dataset> pollutedDatasets = new ArrayList<Dataset>();
+		for(Dataset d: training){
+			if( -1 == d.correctKlass ){
+				pollutedDatasets.add(d);
+			}
+		}
+		training.removeAll(pollutedDatasets);
+		
+		
+		
+		ArrayList<RandomDecisionTree> forest = new ArrayList<RandomDecisionTree>();
+
+		
+		// Add the trees concurrently!
+		ArrayList<AddTreeThread> threads = new ArrayList<AddTreeThread>();
+		for(int i=0; i<threadsN; i++){
+			AddTreeThread t = new AddTreeThread(forest, training);
+			t.start();
+			threads.add(t);
+		}
+		
+		// block till every tree is fully grown :)
+		for(AddTreeThread t: threads){
+			t.join(); 
+		}
+		
+		System.out.println("Forest size: "+forest.size());
+
+
+
+	}
+
+}
