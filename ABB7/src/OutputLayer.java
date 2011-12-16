abb18408b0068a4905cd9b49298d42282a38e0a9
Steffen <stepuh@gmx.de>
2011-12-16 17:25:45 +0100

    bl

--- ABB7/src/OutputLayer.java
+++ ABB7/src/OutputLayer.java
@@ -3,11 +3,15 @@ public class OutputLayer extends Layer{
 
 	public Vector e;
 	
+	
+	// Creates a new Output-Layer that has k nodes
+	// and which's predesessor has prevK nodes
 	public OutputLayer(int prevK, int k){
 		super(prevK, k);
 	}
 	
 	
+	// Calculates the vector of square-differentials between output and teaching
 	public void calcE(Vector teaching){
 		e = (Vector) o.minus(teaching);
 	}
