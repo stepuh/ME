04b2d670a3c3b46b82111d52181640ca599fabc4
Steffen <stepuh@gmx.de>
2011-12-16 21:37:29 +0100

    bla

--- ABB7/src/Client24.java
+++ ABB7/src/Client24.java
@@ -6,21 +6,27 @@ public class Client24 {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
+		// Load patterns
 		ArrayList<Pattern> train = new Reader("pendigits-training.txt").getDatasets();
 		ArrayList<Pattern> test = new Reader("pendigits-testing.txt").getDatasets();
 		int dim = train.get(0).dim;
 		
+		// Create net
 		Net net = new Net(dim, 10);
 		net.addHiddenLayer(16);
 		net.addHiddenLayer(16);
 		net.addHiddenLayer(16);
-		//net.addHiddenLayer(16);
 		
 
-		for(Pattern d: train){
-			net.learnFrom(d);
+		// Lern Patterns
+		int rounds = 2;
+		for(int i=0; i<rounds; i++){
+			for(Pattern d: train){
+				net.learnFrom(d);
+			}
 		}
 
+		// Calculate success rate
 		int total = 0;
 		int correct = 0;
 		for(Pattern d: test){
