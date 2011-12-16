83b1bb13ac3785f00fd757349313d6043c69609b
Sigi <s.gessulat@gmail.com>
2011-12-16 16:39:48 +0100

    calc done

--- ABB7/src/Layer.java
+++ ABB7/src/Layer.java
@@ -39,7 +39,7 @@ public class Layer {
 	
 	
 	
-	public Vector calc( Vector input ){ // without Ext
+	public Vector calc( Vector input ){ // calculates o and derivatives
 		
 		// extend input to input^
 		double[] inputArr = input.toArray();
@@ -50,13 +50,19 @@ public class Layer {
 		inputExtArr[inputArr.length] = 1;
 		Vector inputExt = new Vector(inputExtArr);
 		
+		// calculate o = sigmoid(o^ * w- )
 		Vector tmp = new Vector( inputExt.times(wExt) );
-		
 		o = MathUtil.sigmoid( tmp );
 		
+		// calculate D_j: for diagnoal axis oi^j * (1- oi^j)
+		double[] oArr = o.toArray();
+		derivations = new Matrix(oArr.length, oArr.length);
+		for(int i=0; i<oArr.length; i++){
+			derivations.set(i, i, oArr[i]*(1-oArr[i]));
+		}
 		
-		// TODO:
-		return null;
+		// return o
+		return o;
 	}
 	
 	
