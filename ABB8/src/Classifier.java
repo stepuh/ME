bb79b0e7f44e877960520dfe015444488f6fa045
Steffen <stepuh@gmx.de>
2012-01-09 15:44:10 +0100

    bla

--- ABB8/src/Classifier.java
+++ ABB8/src/Classifier.java
@@ -8,7 +8,7 @@ public class Classifier {
 	public double xStart;
 	public double yStart;
 	public Vector v;
-	
+
 	
 	public Classifier(ArrayList<Pattern> patterns){
 		// Create random vector for classification 
@@ -26,21 +26,53 @@ public class Classifier {
 		// Test perzeptron on all Datasets
 		corrects = 0;
 		for(Pattern p : patterns){
-			// poject vector of pattern on classifier's vector
-			int side = 0;
-			if (vArray[0] * p.features.toArray()[1] - vArray[1]* p.features.toArray()[0] < 0){
-				// links
-				side = 1;
-			}
-			
-			// compare result with teaching
-			if(side == p.teaching){
+			// compare classification with teaching
+			if(classify(p) == p.teaching){
 				corrects++;
 			}
 			
 		}		
+	
+		System.out.print("before corrects: " + corrects);
 		
-		System.out.println("corrects: " + corrects);
+		
+		// Identify liers
+		if(corrects < patterns.size()/2){
+			// turn vector by 180 degrees
+			double xTmp = x2;
+			double yTmp = y2;
+			x2 = xStart;
+			y2 = yStart;
+			xStart = xTmp;
+			yStart = yTmp;
+			
+			vArray[0] = xStart -x2;
+			vArray[1] = yStart -y2;
+			v = new Vector(vArray);
+		}
+
+		corrects = 0;
+		for(Pattern p : patterns){
+			// compare classification with teaching
+			if(classify(p) == p.teaching){
+				corrects++;
+			}
+		}	
+		
+		System.out.println(" | after corrects: " + corrects);
+
 	}
+	
+	
+	public int classify(Pattern p){
+		int side = 0;
+		double[] vArray = v.toArray();
+		if (vArray[0] * p.features.toArray()[1] - vArray[1]* p.features.toArray()[0] < 0){
+			// links
+			side = 1;
+		}
+		return side;
+	}
+	
 	
 }
