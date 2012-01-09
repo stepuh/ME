8f51aedac9b7b9d71937d2f93fdda657d683961a
Sigi <s.gessulat@gmail.com>
2012-01-09 14:25:42 +0100

    reader integriert

--- ABB8/src/Client25.java
+++ ABB8/src/Client25.java
@@ -1,10 +1,16 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class Client25 {
 
 	/**
 	 * @param args
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		
+		ArrayList<Dataset> training = new Reader("ring.csv").getDatasets();
 
 	}
 
