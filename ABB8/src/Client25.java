5291c181e485b6d1d571dfaa6044b6af578379f8
Sigi <s.gessulat@gmail.com>
2012-01-09 14:32:15 +0100

    bla

--- ABB8/src/Client25.java
+++ ABB8/src/Client25.java
@@ -8,9 +8,14 @@ public class Client25 {
 	 * @throws FileNotFoundException 
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
+		int classifierNum = 1000;  // number of classifiers
 		
 		ArrayList<Dataset> training = new Reader("ring.csv").getDatasets();
+		
+		ArrayList<Classifier> classifier = new ArrayList<Classifier>();
+		for(int i=0; i<10; i++){
+			classifier.add( new Classifier(training) ); // adds new random classifier
+		}
 
 	}
 
