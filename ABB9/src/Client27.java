4aecc4835dbbf751492df4004a23ad42c32d3b39
Sigi <s.gessulat@gmail.com>
2012-01-27 14:50:48 +0100

    bla

---
+++ ABB9/src/Client27.java
@@ -0,0 +1,58 @@
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+
+public class Client27 {
+
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+		
+		// read data
+		ArrayList<Pattern> unfiltered = new Reader( "digits-testing-neu.txt" ).getDatasets();
+
+		// drop weird patterns
+		ArrayList<Pattern> training = new ArrayList<Pattern>();
+		for(Pattern p: unfiltered){
+			if( p.teaching != -1){
+				training.add( p );
+			}
+		}
+		
+		NMF myNMF = new NMF(training, 20);
+		
+		myNMF.iterate(300);
+		myNMF.finalNormalizeW();
+		
+		
+		//training = myNMF.getWPatterns();
+		training = myNMF.getWPatterns();
+			
+		int offset_top = -1;
+		int offset_left = 0;
+		int i=0;
+		for(Pattern p: training){
+			p.features.print(1, 1);
+			i++;
+			if(i>20){
+				break;
+			}
+			JFrame jf = new JFrame();
+			jf.setBounds(160, 230, 150, 200);
+			DrawingPanel panel = new DrawingPanel(p);
+			jf.add(panel);
+			jf.repaint();
+			jf.setVisible(true);
+			
+
+		}
+
+	}
+
+}
---
+++ ABB9/src/Client28.java
@@ -0,0 +1,65 @@
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+
+public class Client28 {
+
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+		
+		// read data
+		ArrayList<Pattern> unfiltered = new Reader( "digits-testing-neu.txt" ).getDatasets();
+
+		// drop weird patterns
+		ArrayList<Pattern> training = new ArrayList<Pattern>();
+		for(Pattern p: unfiltered){
+			if( p.teaching != -1){
+				training.add( p );
+			}
+		}
+		
+		NMF myNMF = new NMF(training, 20);
+		
+		myNMF.iterate(300);
+		myNMF.finalNormalizeW();
+		
+		
+		//training = myNMF.getWPatterns();
+		ArrayList<Pattern> reconstructed = new ArrayList<Pattern>();
+		reconstructed = myNMF.getVStrichPatterns();
+			
+
+		int item=12;
+
+		JFrame jfre = new JFrame();
+		jfre.setBounds(160, 230, 150, 200);
+		DrawingPanel panelre = new DrawingPanel(reconstructed.get(item));
+		jfre.add(panelre);
+		jfre.repaint();
+		jfre.setVisible(true);
+		
+		Thread.sleep(100);
+		
+		JFrame jf = new JFrame();
+		jf.setBounds(160, 230, 150, 200);
+		jf.setBackground(Color.WHITE);
+		DrawingPanel panel = new DrawingPanel(training.get(item));
+		jf.add(panel);
+		jf.repaint();
+		jf.setVisible(true);
+		
+		
+			
+
+		
+
+	}
+
+}
